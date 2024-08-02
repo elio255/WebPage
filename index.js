d41da4cd93f7e7b906b5f11a1e9cd1eeae961f67
elio255 <eliomattar00@gmail.com>
2024-08-02 03:57:48 -0900

    Make a Select dynamic with data retreived from API

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 function getStudents(){
     fetch('https://trainingapi-kns1.onrender.com/students')
       .then(response => response.json())
-      .then(json => console.log(json))
+      .then(data => {
+        var select_users = document.getElementById('select_users')
+        data.forEach(st => {
+            var opt = document.createElement('option');
+            opt.innerHTML = st.name;
+        select_users.appendChild(opt);
+        });
+      })
 }
 
 getStudents();
